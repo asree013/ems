6a37bb9ad508bf5ba89f867e5ab107c18521fd71
asree <asree.h@rmutsvmail.com>
2024-06-22 20:19:06 +0700

    ofline first

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
 import withPWAInit from '@ducanh2912/next-pwa';
-/** @type {import('next').NextConfig} */
 
 const nextConfig = {
   output: 'standalone',
 };
-withPWAInit({
+
+export default withPWAInit({
   dest: "public",
-  
-})
-
-export default withPWAInit(nextConfig)
-
-// export default nextConfig;
+  fallbacks: {
+    // Failed page requests fallback to this.
+    document: "/~offline",
+    // This is for /_next/.../.json files.
+    data: "/fallback.json",
+    // This is for images.
+    image: "/fallback.webp",
+    // This is for audio files.
+    audio: "/fallback.mp3",
+    // This is for video files.
+    video: "/fallback.mp4",
+    // This is for fonts.
+    font: "/fallback-font.woff2",
+  },
+})(nextConfig);
