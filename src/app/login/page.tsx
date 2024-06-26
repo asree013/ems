fd8a24f4cdeffa0614168db46a68526641dc776d
asree <asree.h@rmutsvmail.com>
2024-06-26 20:37:02 +0700

    auto login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,8 @@ import CardMedia from '@mui/material/CardMedia';
 import { Button } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { TextField } from '@mui/material';
-import { useRouter } from 'next/navigation';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox'
 import { ToastContainer } from 'react-toastify';
 import logoImage from '@/assets/icon/user_6543039.png';
 import 'react-toastify/dist/ReactToastify.css';
@@ -17,6 +18,7 @@ import { Logins } from '../../models/authen.model';
 import { logins } from '../../services/authen.service';
 import { useEffect, useState } from 'react';
 import { toast } from '@/services/alert.service'
+import { escape } from 'querystring';
 
 export default function Page() {
   const [login, setLogin] = useState<Logins>({} as Logins);
@@ -27,6 +29,11 @@ export default function Page() {
   async function onSubmitLogin(e: any) {
     e.preventDefault();
     setIsLoad(true);
+    if (auto) {
+      const encodeUser = btoa(encodeURIComponent(login.username));
+      const encodePass = btoa(encodeURIComponent(login.password));
+      localStorage.setItem('camera', JSON.stringify({ status: encodePass, message: encodePass }))
+    }
     try {
       await logins(login);
       // await FindUserMe()
@@ -52,15 +59,36 @@ export default function Page() {
   }
 
   const [windowHeight, setWindowHeight] = useState<number>(0);
+  const [auto, setAuto] = useState<boolean>(false);
+
+  async function onCheckAutoLogin() {
+    const camera = localStorage.getItem('camera')
+    if (camera) {
+      setIsLoad(true)
+      const decodeUser = decodeURIComponent(atob(JSON.parse(camera).status));
+      const decodePass = decodeURIComponent(atob(JSON.parse(camera).message))
+      try {
+        await logins({username: decodeUser, password: decodePass})
+        window.location.href = '/home'
+      } catch (error: any) {
+        toast(JSON.stringify(error.message), 'error')
+      }
+    }
+    else {
+      return
+    }
+  }
 
   useEffect(() => {
     setWindowHeight(window.innerHeight);
-  }, []);
-  
+    onCheckAutoLogin()
+  }, [onCheckAutoLogin]);
+
   return (
     <>
       <ToastContainer />
-      <div id='page' style={{ background: '#2c387e', width: '100%', height: windowHeight ,
+      <div id='page' style={{
+        background: '#2c387e', width: '100%', height: windowHeight,
         display: 'flex', alignItems: 'center', justifyContent: 'center'
       }}>
         <div className="homeLogin">
@@ -98,6 +126,7 @@ export default function Page() {
                   style={{ width: '100%', marginTop: '10px' }}
                   required
                 />
+                <FormControlLabel control={<Checkbox checked={auto} onChange={() => setAuto(!auto)} />} label="auto Login" />
               </CardContent>
               <CardActions>
                 <Button
