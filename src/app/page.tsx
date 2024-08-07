a9ce9a3452a7b5f991d7211eaa6d484158a1e4d3
asree <asree.h@rmutsvmail.com>
2024-08-07 18:04:29 +0700

    jing jing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,13 @@
-// pages/some-page.js
+'use client'
+import Loadding from '@/components/Loadding'
+import React, { useEffect } from 'react'
 
-'use client';
-import Loadding from '@/components/Loadding';
-import { useEffect } from 'react';
-
-const page = () => {
-
+export default function App() {
   useEffect(() => {
-    // ลงทะเบียน Service Worker เฉพาะในหน้านี้
-    // if ('serviceWorker' in navigator) {
-    //   window.addEventListener('load', () => {
-    //     const swUrl = '/service-worker.js';
-    //     navigator.serviceWorker.register(swUrl)
-    //       .then(registration => {
-    //         console.log('Service Worker registered with scope:', registration.scope);
-    //       })
-    //       .catch(error => {
-    //         console.error('Service Worker registration failed:', error);
-    //       });
-    //   });
-    // }
     window.location.href = '/login'
-  }, []);
 
+  }, [])
   return (
     <Loadding />
-  );
+  )
 }
-
-export default page;
