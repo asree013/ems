23b4d0a0faa2b07d77dd7140d7fb6a68154bef71
asree <asree.h@rmutsvmail.com>
2024-11-02 17:07:19 +0700

    edit sh file

--- local_for_update.sh
+++ local_for_update.sh
@@ -5,24 +5,24 @@ echo "เริ่มการอัพเดท"
 echo ""
 echo "หยุดการทำงาน...."
 echo ""
-docker compose down
+sudo docker compose down
 
-docker compose pull
+sudo docker compose pull
 
 echo ""
 echo "การอัพเดทเสร็จสิ้น"
 echo ""
 
-docker compose up -d
+sudo docker compose up -d
 
 echo "ลบ Images ที่ไม่ใช้งาน..."
-docker image prune -a -f
+sudo docker image prune -a -f
 
 echo "ลบ Containers ที่ไม่ใช้งาน..."
-docker container prune -f
+sudo docker container prune -f
 
 echo "ลบ Local Volumes ที่ไม่ใช้งาน..."
-docker volume prune -f
+sudo docker volume prune -f
 
 echo ""
 echo "ระบบพร้อมใช่งาน"
